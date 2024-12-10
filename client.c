8fa6732016f7a412794ed715e3238849c7f70cff
Morgan Malie <mmalie@c2r6p6.42nice.fr>
2024-12-10 14:29:45 +0100

    add message inscription (ongoing)

--- client.c
+++ client.c
@@ -21,30 +21,52 @@ int	main(int argc, char **argv)
 		msg = argv[2];
 		sigemptyset(&sigset);
 		sigaddset(&sigset, SIGUSR1);
-	
 		i = 0;
 		while (msg[i] != '\0')
 		{
-			if (msg[i])
-			{
-				res = kill(target_pid, SIGUSR1);
-				if (res == 0)
-					ft_putstr_fd("SIGUSR1 sent\n", 1);
-				else
-					ft_putstr_fd("Error while sending.\n", 2);
-			}
-			else if (msg[i])
-			{
-				res = kill(target_pid, SIGUSR2);
-				if (res == 0)
-					ft_putstr_fd("SIGUSR2 sent\n", 1);
-				else
-					ft_putstr_fd("Error while sending.\n", 2);
-			}
-			i++;
-		}
+			ft_encrypt_char(target_pid, msg[i]);	
+	}
 	return (0);	
 	}
 }
 
+// FROM LIBBIN
+int     ft_is_bit_set(unsigned int num, int bit_pos)
+{
+        int     is_set;
 
+        is_set = 0;
+        if (bit_pos < 0 || ((unsigned int)bit_pos >= (sizeof(num) * 8)))
+                return (-1);
+        if(num & (1 << bit_pos))
+                is_set = 1;
+	return (is_set);
+}
+
+int	ft_encrypt_char(int target_pid, char *character)
+{
+	int	i;
+
+	i = 0;	
+	while (character != '\0')
+	{
+		if (is_set(character) == 0)
+		{
+			res = kill(target_pid, SIGUSR1);
+			if (res == 0)
+				ft_putstr_fd("SIGUSR1 sent\n", 1);
+			else
+				ft_putstr_fd("Error while sending.\n", 2);
+		}
+		else if (is_set(character) == 1)
+		{
+			res = kill(target_pid, SIGUSR2);
+			if (res == 0)
+				ft_putstr_fd("SIGUSR2 sent\n", 1);
+			else
+				ft_putstr_fd("Error while sending.\n", 2);
+		}
+		i++;
+	}
+	return (0);
+}
